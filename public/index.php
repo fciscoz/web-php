2ac9f8c9e28d4bdbedf5148d79f543b2b5093506
fcisco-home <srfrancisco17@gmail.com>
2025-08-18 18:42:18 -0500

    Clase #14

--- public/index.php
+++ public/index.php
@@ -1,20 +1,10 @@
 <?php
 
-require __DIR__.'/../framework/Database.php';
-require __DIR__.'/../framework/Validator.php';
+require __DIR__ . '/../framework/Database.php';
+require __DIR__ . '/../framework/Validator.php';
+require __DIR__ . '/../framework/Router.php';
 
 $db = new Database();
 
-
-$routes = require __DIR__.'/../routes/web.php';
-
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); // <-- Normaliza la URI
-$route = $routes[$requestUri] ?? null;
-
-
-
-if ($route) {
-    require __DIR__.'/../'.$route;
-} else {
-    die("404 Not Found");
-}
+$router = new Router();
+$router->run();
