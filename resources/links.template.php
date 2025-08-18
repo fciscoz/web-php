9c81791edffeebbdea1a421b2b5f463de49083ea
fcisco-home <srfrancisco17@gmail.com>
2025-08-18 09:41:30 -0500

    Add link creation button to links template

--- resources/links.template.php
+++ resources/links.template.php
@@ -26,4 +26,10 @@
 
 </div>
 
+<div class="my-16">
+    <a href="/links/create" class="text-sm font-semibold text-gray-900">
+        Registrar &rarr;
+    </a>
+</div>
+
 <?php require __DIR__.'/partials/footer.php'; ?>
